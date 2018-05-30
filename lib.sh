bd94ea1b27ecb93c103bcbea6bf4d8bb59ec7f4e
peter.pan <peter.pan@gnomito.com>
2018-05-30 15:09:26 +0800

    add hint function

--- lib.sh
+++ lib.sh
@@ -8,4 +8,10 @@ function Parameter_judge() {
     fi
 }
 
-
+function hint() {
+    GREEN='\033[0;32m'
+    NC='\033[0m' # No Color
+    printf "${GREEN}=================================${NC}\n"
+    printf "${GREEN}$1${NC}\n"
+    printf "${GREEN}=================================${NC}\n"
+}
