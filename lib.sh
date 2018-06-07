dc9a125f498dbb60b376f0ceb2f8e3fed77a1694
peter.pan <peter.pan@gnomito.com>
2018-06-07 20:35:32 +0800

    1. replace update with fetch 2. modify the Usage 3. mv List() and Fetch() to lib.sh

--- lib.sh
+++ lib.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+mgr_dir=`cd $(dirname $0);pwd`
+key_dir="$mgr_dir/sshkey"
 
 function Parameter_judge() {
     correct_num="$1"
@@ -14,4 +16,29 @@ function hint() {
     printf "${GREEN}=================================${NC}\n"
     printf "${GREEN}$1${NC}\n"
     printf "${GREEN}=================================${NC}\n"
+}
+
+function List() {
+    cd $key_dir/
+    number=`ls -d */ | wc -l`
+    echo "Total:" $number
+    for key in `ls -d */`
+    do
+        tree $key
+    done
+}
+
+function Fetch() {
+    direction="$1"
+    name="$2"
+
+    if [ "$name" == "all" ];then
+        scp -r $direction/* $key_dir
+        exit 1
+    fi
+
+    host="$(cut -d':' -f1 <<< $direction)"
+    path="$(cut -d':' -f2 <<< $direction)"
+    
+    scp -r $direction/$name $key_dir
 }
