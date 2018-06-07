619db54bc8a29613a3fb87fc0ed8a2f4cbcda6d1
peter.pan <peter.pan@gnomito.com>
2018-06-07 10:07:12 +0800

    add scripts to uninstall openssh

---
+++ uninstall_openssh.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+sudo dpkg -P ssh-import-id
+sudo dpkg -P openssh-server
+sudo dpkg -P openssh-sftp-server
+sudo dpkg -P openssh-client
---
+++ uninstall_ssh_7.4.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd openssh-7.4p1
+sudo make uninstall
+
